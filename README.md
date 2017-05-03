4681ea7b59bde40874a528d18fa1cf7b7d46e38d
Rafał Gierczak <grime85@gmail.com>
2017-05-03 19:22:35 +0200

    Assign animations trigger to element in template

--- README.md
+++ README.md
@@ -13,3 +13,7 @@ To enable Angular Animations do the following steps:
 - `npm install --save @angular/animations`,
 - uncomment in `polyfills.js` line responsible for importing `web-animations-js`,
 - import `BrowserAnimationsModule` in `app.module.ts`
+
+## Which Properties are Animatable ?
+
+Not every CSS property may be animated. The list of animatable properties is available [here](https://www.w3.org/TR/css3-transitions/#animatable-properties).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { trigger, state, style } from '@angular/animations';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.scss'],
+    animations: [
+        trigger('clickedState', [
+            state('default', style({
+                backgroundColor: 'orange',
+                width: '100px',
+                height: '100px'
+            })),
+            state('clicked', style({
+                backgroundColor: 'blue',
+                width: '300px',
+                height: '50px'
+            }))
+        ])
+    ]
 })
 export class AppComponent {
     title = 'app works!';
